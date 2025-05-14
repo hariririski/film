48766a59a6a22a6854d37a0b75558c5964676afc
Hariri Riski <hariririski@gmail.com>
2025-05-14 17:29:28 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -52,7 +52,8 @@ def prepare_files():
         with st.spinner("📥 Mengunduh model dari HuggingFace..."):
             snapshot_download(
                 repo_id="sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2",
-                local_dir=MODEL_PATH
+                local_dir=MODEL_PATH,
+                local_dir_use_symlinks=False
             )
         st.success("✅ Model berhasil diunduh dan siap digunakan.")
 
